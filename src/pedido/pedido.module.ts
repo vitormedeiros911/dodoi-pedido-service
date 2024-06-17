bfa3fa7ca66df82bc0fd2dc89cdd02db0458a647
vitormedeiros911 <vitormedeiros911@outlook.com>
2024-06-17 17:22:47 -0300

    feat: Adiciona criação de pedido

--- src/pedido/pedido.module.ts
+++ src/pedido/pedido.module.ts
@@ -1,12 +1,14 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 
+import { ClientProxyModule } from '../client-proxy/client-proxy.module';
 import { PedidoController } from './pedido.controller';
 import { PedidoService } from './pedido.service';
 import { PedidoSchema } from './schema/pedido.schema';
 
 @Module({
   imports: [
+    ClientProxyModule,
     MongooseModule.forFeature([{ name: 'Pedido', schema: PedidoSchema }]),
   ],
   controllers: [PedidoController],
--- src/pedido/pedido.service.ts
+++ src/pedido/pedido.service.ts
@@ -1,17 +1,32 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
+import { firstValueFrom } from 'rxjs';
 import { v4 as uuid } from 'uuid';
 
+import { ClientProxyService } from '../client-proxy/client-proxy.service';
 import { Pedido } from './schema/pedido.schema';
 
 @Injectable()
 export class PedidoService {
   constructor(
     @InjectModel('Pedido') private readonly pedidoModel: Model<Pedido>,
+    private readonly clientProxyService: ClientProxyService,
   ) {}
 
+  private clientUsuarioBackend =
+    this.clientProxyService.getClientProxyUsuarioServiceInstance();
+
   async criarPedido(pedido: Pedido): Promise<Pedido> {
+    const usuario = await firstValueFrom(
+      this.clientUsuarioBackend.send(
+        'buscar-endereco-por-id-usuario',
+        pedido.idCliente,
+      ),
+    );
+
+    pedido.endereco = usuario.endereco;
+
     const novoPedido = new this.pedidoModel({
       id: uuid(),
       ...pedido,
